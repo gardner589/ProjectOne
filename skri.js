406b3f1207216062d508f0ddf2f9fe4445adad53
Chris Gardner <ctgardner89@gmail.com>
2016-06-07 07:40:54 -0400

    the cards flip, BG thing isnt quite working, still need to add win logic

--- skri.js
+++ skri.js
@@ -4,6 +4,7 @@ var $cfront = $('.cfront');
 var $clicked = $('.clicked');
 var $card = $('.card');
 var c = 0;
+var x = 0;
 var card = $('<div class="card"><div class="cfront"></div><div class="cback"></div></div>');
 var imgs = ["http://puppstheories.com/spriterips/CaptAmericaEagle16f.gif","http://puppstheories.com/spriterips/CaptAmer_hideface_8f.gif","http://puppstheories.com/spriterips/captain-sheildpose_10f.gif","http://puppstheories.com/spriterips/CaptAmer_Star_4.gif","http://puppstheories.com/spriterips/CharlieStance4f.gif","http://puppstheories.com/spriterips/blanka_eatfruit_32f.gif","http://puppstheories.com/spriterips/AkumaStance4f.gif","http://puppstheories.com/spriterips/Colossus_stance_33f.gif"];
 
@@ -11,7 +12,7 @@ function shuffle (array) {
   var i = 0
   var j = 0;
   var temp;
-  for (i = array.length - 1; i > 0; i --) {
+  for (i = array.length -1; i > 0; i --) {
     j = Math.floor(Math.random() * (i + 1))
     temp = array[i]
     array[i] = array[j]
@@ -19,14 +20,13 @@ function shuffle (array) {
   }
 }
 
-
-  for (i = 0; i < imgs.length-2 ;i++){
-  //   shuffle(imgs);
-  // card.clone().css({'background-image': 'url("'+imgs[i]+'")'}).appendTo($board);
-  shuffle(imgs);
-  card.clone().css({'background-image': 'url("'+imgs[i]+'")'}).appendTo($board);
-
+while (x<2){
+  for (i = 0; i < imgs.length ;i++){
+  card.clone().css({'background-image': 'url("'+imgs[i]+'")', 'box-shadow':'-7px -7px 6px #777', 'border': '2px solid white'}).appendTo($board);
   }
+  x++;
+  shuffle(imgs);
+}
 
 $('.card').on('click', function(){
   c ++
@@ -35,4 +35,5 @@ $('.card').on('click', function(){
       c = 1;
     }
   $(this).find('.cback').toggleClass('clicked');
+
 })
